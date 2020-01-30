3782eb4bf3a1e7b8ec4c9aabfcd0bb468b984d8e
Thanh <s3601172@student.rmit.edu.au>
2020-01-30 14:44:45 +1100

    Search and Select

---
+++ src/components/Table.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { useTable, useGlobalFilter, useRowSelect } from "react-table";
+
+const IndeterminateCheckbox = React.forwardRef(
+  ({ indeterminate, ...rest }, ref) => {
+    const defaultRef = React.useRef()
+    const resolvedRef = ref || defaultRef
+
+    React.useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate
+    }, [resolvedRef, indeterminate])
+
+    return (
+      <>
+        <input type="checkbox" ref={resolvedRef} {...rest} />
+      </>
+    )
+  }
+)
+
+
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter
+}) {
+  return (
+    <span>
+      Search:{" "}
+      <input
+        value={globalFilter || ""}
+        onChange={e => {
+          setGlobalFilter(e.target.value || undefined);
+        }}
+      />
+    </span>
+  );
+}
+
+function Table({ columns, data }) {
+  const defaultColumn = React.useMemo(() => ({}), []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn
+    },
+    useGlobalFilter,
+    useRowSelect,
+    hooks => {
+      hooks.flatColumns.push(columns => [
+        {
+          id: 'selection',
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <div>
+              <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+            </div>
+          ),
+          Cell: ({ row }) => (
+            <div>
+              <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+            </div>
+          ),
+        },
+        ...columns,
+      ])
+    }
+  );
+
+  const firstPageRows = rows.slice(0, 10);
+
+  return (
+    <>
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
+
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>Showing the first 10 rows of {rows.length} rows</div>
+    </>
+  );
+}
+
+export default Table;
--- src/components/Topic/TopicList.jsx
+++ src/components/Topic/TopicList.jsx
@@ -3,53 +3,105 @@ import React, { Component } from "react";
 import "./Topic.css";
 
 import Sidebar from "../sidebar/Sidebar";
+import Table from "../Table";
+
+const columns = [
+  { Header: "Topic ID", accessor: "id" },
+  { Header: "Body of Knowledge Reference", accessor: "bokRef" },
+  { Header: "Category", accessor: "category" },
+  { Header: "Topic", accessor: "topic" }
+];
+
 //replace this with db data
 let topicList = [
   {
     id: "CMP.cf.1",
-    bokRef: "MP.cf",
+    bokRef: "ACM-IEEE-SE2014",
     category: "Computing Essentials",
     topic: "Programming Fundamentals (Control and Data Typing)"
   },
   {
+    id: "CMP.cf.2",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Computing Essentials",
+    topic: "Algorithims, Data Structures and Complexity"
+  },
+  {
+    id: "CMP.cf.3",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Computing Essentials",
+    topic: "Problem Solving Techniques"
+  },
+  {
     id: "CMP.ct.1",
-    bokRef: "CMP.ct",
+    bokRef: "ACM-IEEE-SE2014",
     category: "Construction Technologies",
     topic: "API Design and Use"
+  },
+  {
+    id: "CMP.ct.2",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Construction Technologies",
+    topic: "Code Reuse and Libraries"
+  },
+  {
+    id: "CMP.ct.3",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Construction Technologies",
+    topic: "Object-Oriented Runtime Issues (e.g., polymorphism and dynamic...)"
+  },
+  {
+    id: "FND.mf.1",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Mathematical and Engineering Fundamentals",
+    topic: "Functions, Relations and Sets"
+  },
+  {
+    id: "FND.mf.2",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Mathematical and Engineering Fundamentals",
+    topic: "Basic Logic (Propositional and Predicate)"
+  },
+  {
+    id: "FND.mf.3",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Mathematical and Engineering Fundamentals",
+    topic: "Proof Techniques (Direct, Contradiction, and Inductive)"
+  },
+  {
+    id: "PRF.psy.1",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Professional Practice",
+    topic: "Dynamics of Working in Teams and Groups"
+  },
+  {
+    id: "PRF.psy.2",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Professional Practice",
+    topic: "Individual Cognition (e.g., limits)"
+  },
+  {
+    id: "PRF.psy.3",
+    bokRef: "ACM-IEEE-SE2014",
+    category: "Professional Practice",
+    topic: "Cognitive Problem Complexity"
   }
 ];
 
+
 class TopicList extends Component {
-  render() {
-    return (
-      <div className="centre">
+  render(){
+    return(
+      <div className = "centre">
         <Sidebar />
         <h1>Topic List</h1>
-        <input type="text" name="search"></input>
-        <button type="submit">Search</button>
-        <table>
-          <thead>
-            <tr>
-              <th>Topic ID</th>
-              <th>Body of Knowledge Reference</th>
-              <th>Category</th>
-              <th>Topic</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {topicList.map(topics => (
-              <tr>
-                <td>{topics.id}</td>
-                <td>{topics.bokRef}</td>
-                <td>{topics.category}</td>
-                <td>{topics.topic}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <Table columns = {columns} data={topicList}/>
+        <button type="submit">Add Topic</button>
       </div>
-    );
+    )
   }
 }
+
+
+
 export default TopicList;
