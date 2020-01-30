5d08b8c5339bd6582f1ef78e60ac5774ed268c0e
s3699661 <>
2020-01-30 21:23:59 +1100

    testv3.0

--- src/components/addtopic/Addtopic.jsx
+++ src/components/addtopic/Addtopic.jsx
@@ -3,23 +3,13 @@ import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom'
 import "./Addtopic.css"
 import Sidebar from "../sidebar/Sidebar";
 
-class addT extends Component{
-    render() {
-        return(
-            <div className="addTopic">
-            <Sidebar/>
-            </div>
-        )
-    }
-}
-
 class add extends Component{
     constructor(props){
         super(props)
         this.state = {
             todos :
             [
-                {BOKRr:'Test',Category:'Computing Essentials' ,TopicID:'1', Topic:'Test',PreReq:'Test',Outcome:'test',Hour:'5'}
+                {BOKRr:'Test',Category:'Computing Essentials' ,TopicID:'1', Topic:'Test',PreReq:'Test',Outcome:'test',Hours:'5'}
             ]
         }
     }
@@ -44,14 +34,14 @@ class add extends Component{
                         <tbody>
                             {
                                 this.state.todos.map(
-                                    todo =>
+                                    title =>
                                         <tr>
-                                            <td>{todo.BOKRr}</td>
-                                            <td>{todo.Category}</td>
-                                            <td>{todo.TopicID}</td>
-                                            <td>{todo.PreReq}</td>
-                                            <td>{todo.Outcome}</td>
-                                            <td>{todo.Hours}</td>
+                                            <td>{title.BOKRr}</td>
+                                            <td>{title.Category}</td>
+                                            <td>{title.TopicID}</td>
+                                            <td>{title.PreReq}</td>
+                                            <td>{title.Outcome}</td>
+                                            <td>{title.Hours}</td>
                                         </tr>
                                 )
                             }
@@ -61,14 +51,6 @@ class add extends Component{
                     <button className="btn btn-outline-primary" >Add Topic</button>
                 </div>
             </div>
-        )
-    }
-}
-
-class addTp extends Component{
-    render() {
-        return (
-            <div>TopicList</div>
         )
     }
 }
