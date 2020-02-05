6004de6a8b930b41d9c2984b840d6c775db6fe54
Thanh <s3601172@student.rmit.edu.au>
2020-02-05 23:43:00 +1100

    inital rewrite no react-table

--- src/components/topicList/TopicList.jsx
+++ src/components/topicList/TopicList.jsx
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import TopicService from "../API/topic/TopicService.js";
 
 import "../../bootstrap.css";
 import "../../index.css";
 
-import Sidebar from "../sidebar/Sidebar";
-import SearchableTable from "../searchableTable/SearchableTable";
-
-const columns = [
-  { Header: "Body of Knowledge Reference", accessor: "bokRef" },
-  { Header: "Area", accessor: "area" },
-  { Header: "Unit", accessor: "unit" },
-  { Header: "Topic ID", accessor: "id" },
-  { Header: "Topic", accessor: "topic" }
-];
-
 class TopicList extends Component {
-  constructor(props){
-      super(props);
-      this.state = {
-        topicList: []
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      topicList: [],
+      total: null,
+      perPage: 10,
+      currentPage: 1
+    };
+    
+    this.renderTableData = this.renderTableData.bind(this);
+    this.refreshList = this.refreshList.bind(this);
   }
 
   componentDidMount() {
@@ -35,22 +30,82 @@ class TopicList extends Component {
     })
   }
 
-  addTopicBtn(){
-    this.props.history.push("/add-topic");
+  renderTableData() {
+    return this.state.topicList.map((topicList, index) => {
+      const { bokRef, area, unit, id, topic} = topicList;
+      return (
+        <tr key={id}>
+          <td><input type="checkbox"></input></td>
+          <td>{bokRef}</td>
+          <td>{area}</td>
+          <td>{unit}</td>
+          <td>{topic}</td>
+          <td>
+              <select className="custom-select">
+              <option value="remembering">Remembering</option>
+              <option value="understanding">Understanding</option>
+              <option value="applying">Applying</option>
+              <option value="analyzing">Analyzing</option>
+              <option value="evaluating">Evaluating</option>
+              <option value="creating">Creating</option>
+              </select>
+          </td>
+          <td>
+            <select className="custom-select">
+            <option value="remembering">Remembering</option>
+            <option value="understanding">Understanding</option>
+            <option value="applying">Applying</option>
+            <option value="analyzing">Analyzing</option>
+            <option value="evaluating">Evaluating</option>
+            <option value="creating">Creating</option>
+            </select>
+          </td>
+        </tr>
+      );
+    });
   }
 
   render() {
-    return (
-      <div className="centre">
-        <Sidebar />
+    return( 
+      <div className="container centre">
         <h1>Topic List</h1>
-        <SearchableTable columns={columns} data={this.state.topicList} />
-        <button className="btn btn-outline-dark" type="submit">
-          Add Topic
-        </button>
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">Search</span>
+          <input type="text" className="form-control"></input>
+        </div>
+      
+        <table className="table table-striped table-bordered">
+          <thead className="thead-dark">
+            <tr>
+              <th><input type="checkbox"></input></th>
+              <th>Book of Knowledge Reference</th>
+              <th>Area</th>
+              <th>Unit</th>
+              <th>Topic Name</th>
+              <th>Prerequisite Level</th>
+              <th>Outcome Level</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.renderTableData()}             
+          </tbody>
+        </table>
+
+        <div className="pagination">
+          <span className="page-item page-link">&laquo;</span>
+          <span className="page-item page-link">1</span>
+          <span className="page-item page-link">2</span>
+          <span className="page-item page-link">3</span>
+          <span className="page-item page-link">4</span>
+          <span className="page-item page-link">&raquo;</span>
+        </div>
+          <button className="btn btn-outline-dark">Add Topic(/s)</button>
       </div>
+      
     );
   }
 }
 
 export default TopicList;
+
