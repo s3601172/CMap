2fc1cb9692ad9e8b26604e9c9d7572aeab764522
Thanh <s3601172@student.rmit.edu.au>
2020-02-06 01:55:26 +1100

    minor edits

--- src/components/topicList/TopicList.jsx
+++ src/components/topicList/TopicList.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import TopicService from "../API/topic/TopicService.js";
+import Pagination from "react-bootstrap/Pagination";
 
 import "../../bootstrap.css";
 import "../../index.css";
@@ -8,14 +9,12 @@ class TopicList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topicList: [],
-      total: null,
-      perPage: 10,
-      currentPage: 1
+      topicList: []
     };
     
     this.renderTableData = this.renderTableData.bind(this);
     this.refreshList = this.refreshList.bind(this);
+    this.selectId = this.selectId.bind(this);
   }
 
   componentDidMount() {
@@ -30,17 +29,29 @@ class TopicList extends Component {
     })
   }
 
+  
+
   renderTableData() {
     return this.state.topicList.map((topicList, index) => {
       const { bokRef, area, unit, id, topic} = topicList;
       return (
-        <tr key={id}>
-          <td><input type="checkbox"></input></td>
-          <td>{bokRef}</td>
-          <td>{area}</td>
-          <td>{unit}</td>
-          <td>{topic}</td>
-          <td>
+          <tr key={id}>
+            <td><input type="checkbox"></input></td>
+            <td>{bokRef}</td>
+            <td>{area}</td>
+            <td>{unit}</td>
+            <td>{topic}</td>
+            <td>
+                <select className="custom-select">
+                <option value="remembering">Remembering</option>
+                <option value="understanding">Understanding</option>
+                <option value="applying">Applying</option>
+                <option value="analyzing">Analyzing</option>
+                <option value="evaluating">Evaluating</option>
+                <option value="creating">Creating</option>
+                </select>
+            </td>
+            <td>
               <select className="custom-select">
               <option value="remembering">Remembering</option>
               <option value="understanding">Understanding</option>
@@ -49,58 +60,51 @@ class TopicList extends Component {
               <option value="evaluating">Evaluating</option>
               <option value="creating">Creating</option>
               </select>
-          </td>
-          <td>
-            <select className="custom-select">
-            <option value="remembering">Remembering</option>
-            <option value="understanding">Understanding</option>
-            <option value="applying">Applying</option>
-            <option value="analyzing">Analyzing</option>
-            <option value="evaluating">Evaluating</option>
-            <option value="creating">Creating</option>
-            </select>
-          </td>
-        </tr>
+            </td>
+          </tr>
       );
     });
   }
 
   render() {
     return( 
-      <div className="container centre">
+      <div className="container centre mt-5">
         <h1>Topic List</h1>
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">Search</span>
+        <div className="input-group-prepend mt-4 mb-4">
+          <span className="input-group-text">Search</span>
           <input type="text" className="form-control"></input>
         </div>
       
-        <table className="table table-striped table-bordered">
-          <thead className="thead-dark">
-            <tr>
-              <th><input type="checkbox"></input></th>
-              <th>Book of Knowledge Reference</th>
-              <th>Area</th>
-              <th>Unit</th>
-              <th>Topic Name</th>
-              <th>Prerequisite Level</th>
-              <th>Outcome Level</th>
-            </tr>
-          </thead>
+        <form>
+          <table className="table table-striped table-bordered">
+            <thead className="thead-dark">
+              <tr>
+                <th><input type="checkbox"></input></th>
+                <th>Book of Knowledge Reference</th>
+                <th>Area</th>
+                <th>Unit</th>
+                <th>Topic Name</th>
+                <th>Prerequisite Level</th>
+                <th>Outcome Level</th>
+              </tr>
+            </thead>
 
-          <tbody>
-            {this.renderTableData()}             
-          </tbody>
-        </table>
+            <tbody>
+              {this.renderTableData()}             
+            </tbody>
+          </table>
 
-        <div className="pagination">
-          <span className="page-item page-link">&laquo;</span>
-          <span className="page-item page-link">1</span>
-          <span className="page-item page-link">2</span>
-          <span className="page-item page-link">3</span>
-          <span className="page-item page-link">4</span>
-          <span className="page-item page-link">&raquo;</span>
-        </div>
-          <button className="btn btn-outline-dark">Add Topic(/s)</button>
+          <Pagination>
+            <Pagination.First />
+            <Pagination.Prev />
+            <Pagination.Item>{1}</Pagination.Item>
+            <Pagination.Item>{2}</Pagination.Item>
+            <Pagination.Item>{3}</Pagination.Item>
+            <Pagination.Next />
+            <Pagination.Last />
+          </Pagination>
+          <button type="submit" className="btn btn-outline-dark">Add Topic(/s)</button>
+        </form>
       </div>
       
     );
