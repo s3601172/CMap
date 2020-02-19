ad5b0a2fce704285622b19e006beca035ff987f2
Thanh <s3601172@student.rmit.edu.au>
2020-02-19 17:06:57 +1100

    path change

--- src/components/API/program/ProgramService.js
+++ src/components/API/program/ProgramService.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 class ProgramService {
   getProgramList() {
-    return axios.get("http://localhost:8080/code/BP094GEN8/year/2018");
+    return axios.get("http://cmapbackend-env.dz3ak2mbhv.ap-southeast-2.elasticbeanstalk.com/code/BP094GEN8/year/2018");
   }
 
   getProgramCourseList() {
-    return axios.get("http://localhost:8080/allCourseinProgram/BP094GEN8");
+    return axios.get("http://cmapbackend-env.dz3ak2mbhv.ap-southeast-2.elasticbeanstalk.com/allCourseinProgram/BP094GEN8");
   }
 
   deleteCourse(course) {
     console.log("delete");
-    return axios.delete("http://localhost:8080/delete-program-course", course);
+    return axios.delete("http://cmapbackend-env.dz3ak2mbhv.ap-southeast-2.elasticbeanstalk.com/delete-program-course", course);
   }
 
   updateCourse(course) {
     console.log("update");
-    return axios.put("http://localhost:8080/update-program-course", course);
+    return axios.put("http://cmapbackend-env.dz3ak2mbhv.ap-southeast-2.elasticbeanstalk.com/update-program-course", course);
   }
 }
 
